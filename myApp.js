dd40f626c8bfaefa745be04a17eed65523314a24
LeDu <lequangdu.cv@gmail.com>
2023-04-10 14:07:03 +0700

    fixed conflict

--- myApp.js
+++ myApp.js
@@ -3,7 +3,11 @@ let app = express();
 
 console.log("Hello World")
 console.log("make some changes in remote")
+<<<<<<< Updated upstream
 console.log("Add some changes to remote")
+=======
+console.log("Add change in local")
+>>>>>>> Stashed changes
 
 
 
